e6803fe9de544062f2bfa6527b80062588ce0a50
Devon Tingley <dtingley@twilit.io>
2021-03-16 09:32:14 -0400

    Add functionality for get, put, and update

--- src/db.rs
+++ src/db.rs
@@ -20,15 +20,41 @@ struct SecondaryNode {
 
 impl Database {
     fn put(&mut self, prime_key: String, second_key: String, value: String) {
-        // 
+        let inner_node = SecondaryNode{
+            key: second_key.clone(),
+            value: value.clone()
+        };
+
+        match self.store.get_mut(prime_key.as_str()) {
+            Some(node) => {
+                node.value.insert(second_key, inner_node);
+            },
+
+            // Creates a new Primary Node since one doesn't exist
+            None => {
+                let mut inner_store: HashMap<String, SecondaryNode> = HashMap::new();
+                inner_store.insert(second_key, inner_node);
+
+                let prime_node = PrimaryNode{
+                    key: prime_key.clone(),
+                    value: inner_store,
+                };
+
+                self.store.insert(prime_key, prime_node);
+            }
+        }
     }
 
-    fn get(&self, prime_key: String, second_key: String) -> Option<SecondaryNode> {
-        // Grab from the primary or secondary key
-        None
+    fn get(&self, prime_key: String, second_key: String) -> String {
+        let prime = self.store.get(prime_key.as_str());
+
+        match prime {
+            Some(node) => unwrap(node.value.get(second_key.as_str())),
+            None => String::from("null"),
+        }
     }
 
-    fn update(&self, prime_key: String, second_key: String, value: String) {
+    fn update(&mut self, prime_key: String, second_key: String, value: String) {
         self.put(prime_key, second_key, value);
     }
 
@@ -50,9 +76,9 @@ fn input() -> String {
 }
 
 
-fn unwrap(result: Option<&String>) -> String {
+fn unwrap(result: Option<&SecondaryNode>) -> String {
     match result {
-        Some(item) => (*item).clone(),
+        Some(item) => (*item).value.clone(),
         None => String::from("nil")
     }
 }
@@ -68,10 +94,14 @@ pub fn start() {
         // let cmd = input();
         let prime_operator: Option<&str> = Some("Hello");
 
+        let prime_key = String::from("Hello");
+        let second_key = String::from("there");
+        let value = String::from("world");
+
         match prime_operator {
-            Some("put")    => print!("Put"),
-            Some("get")    => print!("Get"),
-            Some("update") => print!("Update"),
+            Some("put")    => db.put(prime_key, second_key, value),
+            Some("get")    => println!("{}", db.get(prime_key, second_key)),
+            Some("update") => db.update(prime_key, second_key, value),
             Some("delete") => print!("Delete"),
             Some("exit")   => exit(),
             Some(_)        => println!("error: could not read command"),
